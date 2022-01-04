c07bf5fe09aac21876865860e607a825174ea5ae
Alex 'Avunia' Takiya <6952402+atakiya@users.noreply.github.com>
2022-01-04 16:41:59 +0100

    Adds docker builds configuration, workflow and bootstrapper (#1)
    * Add dockerfile and entry script for container
    
    * Add apt-get gather job for dos2unix
    
    * Move dos2unix to build step
    
    As originally intended, oops.
    
    * Pass commandline parameters to watchdog executable
    
    * Add workflow for building and publishing an image to ghcr
    
    * Quote params passed to watchdog to avoid globbing
    
    * Add more info on what the bootstrap is doing
    
    * Add log prefix and helper function, little surprise
    
    * Switch app image to alpine, add libs, envvars, workdir
    
    Reduces image size by about 70MB
    
    Adds libs required to get Ian working on alpine
    
    Switches to /app as workdir
    
    Adds envvar for watchdog address
    
    Changes shell to sh for broader compat
    
    * Fix the k9 surprise, proper randomizer script
    
    * Add missing print statement, oops
    
    * Add verbose to command output
    
    * Add exclamation mark and newline at last message
    
    * Replace echo with printf
    
    * Add force flag to volatile config removal

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+
+RUN apt-get update \
+  && apt-get install -y dos2unix
+
+WORKDIR /build
+
+COPY SS14.Watchdog/ .
+
+RUN dotnet restore
+
+RUN dotnet publish -c Release -r linux-x64 --no-self-contained
+
+RUN cp -R bin/Release/net6.0/linux-x64/publish /app
+RUN cp appsettings.Development.yml /app/appsettings.yml
+
+COPY ian.sh ian.sh
+RUN dos2unix ian.sh
+
+
+FROM mcr.microsoft.com/dotnet/aspnet:6.0-alpine AS app
+
+RUN apk update && apk add --no-cache \
+  ca-certificates \
+  gcompat \
+  icu-libs
+
+COPY --from=build /app /app
+COPY --from=build /build/ian.sh /app/ian.sh
+
+WORKDIR /app
+
+EXPOSE 1212/tcp
+EXPOSE 1212/udp
+
+ENV ASPNETCORE_URLS=http://+:5000
+ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=false
+
+VOLUME ["/app/instances", "/app_config"]
+
+ENTRYPOINT ["/bin/sh", "./ian.sh"]
---
+++ ian.sh
@@ -0,0 +1,44 @@
+# App config in container volume
+CONFIG_VOLUME="/app_config/appsettings.yml"
+# App config in volatile storage
+CONFIG_TARGET="/app/appsettings.yml"
+
+# The things Ian can say, one will be picked at random
+IAN_NOISE=`echo "Woof Bork Bark Wau Ruff Hauhau Awroo" |
+  awk 'BEGIN { srand() } {split($0,a); print a[1+int(rand()*length(a))]}'`
+
+bs_print () { printf '%s\n' "[IAN BOOTSTRAP] ""$@"""; }
+
+printf '%s\n' "
+  ######\  ######\  ##\   ##\ 
+  \_##  _|##  __##\ ###\  ## |
+    ## |  ## /  ## |####\ ## |   The Nanotrasen approved
+    ## |  ######## |## ##\## |        Space Station 14
+    ## |  ##  __## |## \#### |      Watchdog
+    ## |  ## |  ## |## |\### |
+  ######\ ## |  ## |## | \## |   Horizon Container Edition
+  \______|\__|  \__|\__|  \__|
+"
+
+if [ ! -f "$CONFIG_VOLUME" ]; then
+  bs_print "Daemon configuration not found, copying from deployment..."
+  # Copy fresh config to mount
+  cp -v $CONFIG_TARGET $CONFIG_VOLUME
+fi
+
+# Delete the fresh config
+bs_print "Wiping volatile config..."
+rm -vf $CONFIG_TARGET
+
+# Symlink the real config to the app dir
+bs_print "Linking mounted config to watchdog..."
+ln -vsf $CONFIG_VOLUME $CONFIG_TARGET
+
+# Check if we succeeded with creating the link
+if [ ! -f "$CONFIG_TARGET" ]; then
+  bs_print "Failed to create symlink for config!"
+  exit 1
+fi
+
+bs_print "Handing over to Ian! $IAN_NOISE!" ''
+exec ./SS14.Watchdog "$@"
