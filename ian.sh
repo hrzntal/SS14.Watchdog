e8b5ee70cd538bf3395afe137651139623a76aa9
Alex 'Avunia' Takiya <git@takiya.eu>
2022-01-04 06:10:17 +0100

    Fix the k9 surprise, proper randomizer script

--- ian.sh
+++ ian.sh
@@ -3,23 +3,21 @@ CONFIG_VOLUME="/app_config/appsettings.yml"
 # App config in volatile storage
 CONFIG_TARGET="/app/appsettings.yml"
 
-# The things Ian can say
-IAN_NOISES=("Woof" "Bork" "Bark" "Wau" "Ruff" "Hau Hau" "Awroo")
-# Random number from 0 to 32767
-RANDOM=$$$(date +%s)
-DOG_NOISE=${IAN_NOISES[$RANDOM % ${#RANDOM[@]}]}
+# The things Ian can say, one will be picked at random
+IAN_NOISE=`echo "Woof Bork Bark Wau Ruff Hauhau Awroo" |
+  awk 'BEGIN { srand() } {split($0,a); a[1+int(rand()*length(a))]}'`
 
-bs_print () { echo "[IAN BOOTSTRAP] "$@"" }
+bs_print () { echo "[IAN BOOTSTRAP] ""$@"""; }
 
-bs_print "
-######\  ######\  ##\   ##\ 
-\_##  _|##  __##\ ###\  ## |
-  ## |  ## /  ## |####\ ## |   The Nanotrasen approved
-  ## |  ######## |## ##\## |        Space Station 14
-  ## |  ##  __## |## \#### |      Watchdog
-  ## |  ## |  ## |## |\### |
-######\ ## |  ## |## | \## |   Horizon Container Edition
-\______|\__|  \__|\__|  \__|
+echo "
+  ######\  ######\  ##\   ##\ 
+  \_##  _|##  __##\ ###\  ## |
+    ## |  ## /  ## |####\ ## |   The Nanotrasen approved
+    ## |  ######## |## ##\## |        Space Station 14
+    ## |  ##  __## |## \#### |      Watchdog
+    ## |  ## |  ## |## |\### |
+  ######\ ## |  ## |## | \## |   Horizon Container Edition
+  \______|\__|  \__|\__|  \__|
 "
 
 if [ ! -f "$CONFIG_VOLUME" ]; then
@@ -42,5 +40,5 @@ if [ ! -f "$CONFIG_TARGET" ]; then
   exit 1
 fi
 
-bs_print "Handing over to Ian! $DOG_NOISE"
+bs_print "Handing over to Ian! $IAN_NOISE"
 exec ./SS14.Watchdog "$@"
